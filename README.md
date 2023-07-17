d1fe89d227224f79caa69b1fec534cfca69a1c6c
David Richards <richards.malton@gmail.com>
2023-07-17 12:58:37 +0100

    Added TFT display
    update readme with video link
    update software to drive LCD display

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # Aviation_Synchro_Decoder
 Decode angle of synchro transmitter using Arduino RP2040
 
+##
+
+Synchro transmitter being read by raspberry pi pico rp2040 arduino sketch with output to TFT display. Software is reading the synchro reference, and two phases of the stator. The method used for decoding is  a tracking synchro to digital converter.
+
 ## software
 
-[rp2040 sketch](./software/pico_measure_synchro/pico_measure_synchro.ino)
+[rp2040 sketch](./software/pico_synchro_display/pico_synchro_display.ino)
+
+## youtube video
+
+[synchro angle display](https://youtube.com/shorts/Mp-xyvLs0cE)
 
 ## pictures
 
--- software/pico_synchro_display/pico_synchro_display.ino
+++ software/pico_synchro_display/pico_synchro_display.ino
@@ -1,37 +1,110 @@
 /*
- * Read synchro transmitter angle, display angle on serial console or plotter
- * djrm: July 16 2023, this version appears to work!
- * Target rp2040 with three channel ADC inputs fed from referencew and two synchro outputs, third op common.
- * Tracking Synchro-to-Digital Converter
- * method used copied from here: https://bikerglen.com/blog/building-a-synchro-to-digital-converter/
- *
-*/
 
+ Make sure all the display driver and pin connections are correct by
+ editing the User_Setup.h file in the TFT_eSPI library folder.
+
+ #########################################################################
+ ###### DON'T FORGET TO UPDATE THE User_Setup.h FILE IN THE LIBRARY ######
+ #########################################################################
+ 
+ */
+
+#include <SPI.h>
+#include <TFT_eSPI.h> // Hardware-specific library
 #include <ADCInput.h>         // buffered ADC read stream
 #include <math.h>             // initial build using floating point, intent to rewrite with fixed point
 
 #define ADC_READ_FREQ 10000   // frequency of adc reading (per each channel)
 #define ADC_OFFSET 2064       // vause with no input present
-#define PIN_LCD_CE 17         // used to disable LCD on pico explorer board
 
 ADCInput ADCInputs(A0, A1, A2);
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
 
-void setup() {
-  // run once:
-  Serial.begin(115200);
-  //pinMode(PIN_DEBUG, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+#define TFT_GREY 0x5AEB
+float sx = 0, sy = 1;    // Saved H, M, S x & y multipliers
+//float sdeg=0;
+uint16_t osx=120, osy=120;  // Saved H, M, S x & y coords
+uint16_t x0=0, x1=0, yy0=0, yy1=0;
+uint8_t ss=0;
 
-  // disable onboard SPI display
-  pinMode(PIN_LCD_CE, OUTPUT);  
-//  digitalWrite(PIN_LCD_CE, HIGH);
+uint32_t targetTime = 0;                    // for next timeout
+float angle;
+
+void setup(void) {
+  tft.init();
+  tft.setRotation(0);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE, TFT_GREY);  // Adding a background colour erases previous text automatically
   
+  Serial.begin(115200);
+  while(!Serial);
   ADCInputs.begin(ADC_READ_FREQ);
+
+  // Draw clock face
+  tft.fillCircle(120, 120, 118, TFT_WHITE);
+  tft.fillCircle(120, 120, 110, TFT_BLACK);
+
+  // Draw 12 lines
+  for(int i = 0; i<360; i+= 5) {
+    sx = cos((i-90)*0.0174532925);
+    sy = sin((i-90)*0.0174532925);
+    x0 = sx*110+120;
+    yy0 = sy*110+120;
+    x1 = sx*108+120;
+    yy1 = sy*108+120;
+
+    tft.drawLine(x0, yy0, x1, yy1, TFT_WHITE);
+  }
+  // Draw 12 lines
+  for(int i = 0; i<360; i+= 30) {
+    sx = cos((i-90)*0.0174532925);
+    sy = sin((i-90)*0.0174532925);
+    x0 = sx*114+120;
+    yy0 = sy*114+120;
+    x1 = sx*100+120;
+    yy1 = sy*100+120;
+
+    tft.drawLine(x0, yy0, x1, yy1, TFT_WHITE);
+  }
+
+
+  // Draw 60 dots
+  for(int i = 0; i<360; i+= 10) {
+    sx = cos((i-90)*0.0174532925);
+    sy = sin((i-90)*0.0174532925);
+    x0 = sx*102+120;
+    yy0 = sy*102+120;
+     // Draw minute markers
+    tft.drawPixel(x0, yy0, TFT_WHITE);
+
+    // Draw main quadrant dots
+    if(i==0 || i==180) tft.fillCircle(x0, yy0, 2, TFT_WHITE);
+    if(i==90 || i==270) tft.fillCircle(x0, yy0, 2, TFT_WHITE);
+  }
+
+  tft.fillCircle(120, 121, 3, TFT_WHITE);
+
+  targetTime = millis() + 1000; 
+  draw_needle(0);
+
+}
+
+void draw_needle(float sdeg)
+{
+      // Pre-compute hand degrees, x & y coords for a fast screen update
+    sx = cos((sdeg-90)*0.0174532925);    
+    sy = sin((sdeg-90)*0.0174532925);
+
+    // Redraw new hand positions, hour and minute hands not erased here to avoid flicker
+    tft.drawLine(osx, osy, 120, 121, TFT_BLACK);
+    osx = sx*99+121;    
+    osy = sy*99+121;
+    tft.drawLine(osx, osy, 120, 121, TFT_RED);
+    tft.fillCircle(120, 121, 3, TFT_RED);
 
 }
 
 void loop() {
-
   int sinVal;
   int cosVal;
   int refVal;
@@ -49,8 +122,8 @@ void loop() {
   // run repeatedly:  read the ADC. ( 0 to 4095 )
   // make value signed +- 2048 by applying offset
   refVal=ADCInputs.read()-ADC_OFFSET; 
-  sinVal=ADCInputs.read()-ADC_OFFSET;
   cosVal=ADCInputs.read()-ADC_OFFSET;
+  sinVal=ADCInputs.read()-ADC_OFFSET;
 
   // note that s2ms1 isn't actually used
   // convert to float in range -2PI to +2PI
@@ -67,22 +140,16 @@ void loop() {
   else
       refsqwv = 0;
 
+#if 0
   if(refsqwv>0)
     digitalWriteFast(LED_BUILTIN, HIGH);
   else if (refsqwv<0)
-    digitalWriteFast(LED_BUILTIN, LOW);                                
-
-#if 0
-  // show led when ref present
-  if(refVal>0)
-    digitalWriteFast(LED_BUILTIN, HIGH);
-  else if(refVal<0)
     digitalWriteFast(LED_BUILTIN, LOW);                                
 #endif
 
 #if 0
   // scott t transform the inputs
-  sinin = s1ms3;
+  sinin = -s1ms3;
   // cosin = 2/sqrt(3) * (s3ms2 + 0.5 * s1ms3)
   cosin = 1.1547 * (s3ms2 + 0.5 * s1ms3);
 #else  
@@ -106,14 +173,10 @@ void loop() {
 
 
   { // angle display update every 250mS
-    static int del_count = 0;
-    int del_value;
-    float angle;
-    del_value = millis();
-    if(del_value - del_count > 250)
-    {
-      del_count = del_value;
-      digitalWriteFast(LED_BUILTIN, LOW); 
+  if (targetTime < millis()) 
+  {
+    targetTime += 250;
+
   #if 0                                   
   //    Serial.print("ref=");
       Serial.print(refVal);
@@ -152,8 +215,10 @@ void loop() {
       Serial.print(demod,2);
       Serial.print(", ");
   #endif
+
       angle = theta*180/M_PI ;
-      angle = fmod(angle+180,360);
+      angle = fmod(angle+180.0,360.0);
+#if 1
   //    Serial.print("theta=");
   //    Serial.print(theta,2);
   //    Serial.print(theta_,2);
@@ -161,9 +226,10 @@ void loop() {
   //    Serial.print("angle=");
       Serial.print(angle,2);
       Serial.println();
+#endif
+
+      draw_needle(angle);
+
     }
   }
-
 }
-
-// end of pico_measure_synchro
